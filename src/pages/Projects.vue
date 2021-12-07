0ef829901a6cd1028a61ea13757954f7adadb356
Malkoc Mirza <81041253+Mirza-bot@users.noreply.github.com>
2021-12-07 19:29:56 +0100

    finished projects page

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -33,14 +33,26 @@
         </ul>
         <template #actions>
           <div class="spacer"></div>
+          <a
+            href="https://kanban-board-91d98.web.app/"
+            rel="noopener"
+            target="_blank"
+          >
           <w-button class="mx5 mt5" xl bg-color="orange-light2" color="black"
             ><w-icon class="mr2" xl color="black">mdi mdi-desktop-mac</w-icon>
             Demo</w-button
+          >
+          </a>
+          <a
+            href="https://github.com/Mirza-bot/Kanban-board/tree/main/kanban-board"
+            rel="noopener"
+            target="_blank"
           >
           <w-button class="mx8 mt5" xl bg-color="orange-light2" color="black"
             ><w-icon class="mr2" xl color="black">mdi mdi-code-braces</w-icon
             >Code</w-button
           >
+          </a>
         </template>
       </w-card>
     </div>
@@ -70,20 +82,33 @@
         </ul>
         <template #actions>
           <div class="spacer"></div>
+          <a
+            href="https://pietro-s-pizzeria.web.app/main"
+            rel="noopener"
+            target="_blank"
+          >
           <w-button class="mx5 mt5" xl bg-color="orange-light2" color="black"
             ><w-icon class="mr2" xl color="black">mdi mdi-desktop-mac</w-icon>
             Demo</w-button
           >
-          <w-button class="mx8 mt5" xl bg-color="orange-light2" color="black"
-            ><w-icon class="mr2" xl color="black">mdi mdi-code-braces</w-icon
-            >Code</w-button
+          </a>
+          <a
+            href="https://github.com/Mirza-bot/pietro_pizzeria"
+            rel="noopener"
+            target="_blank"
           >
+            <w-button class="mx8 mt5" xl bg-color="orange-light2" color="black">
+              <w-icon class="mr2" xl color="black">mdi mdi-code-braces</w-icon
+              >Code
+            </w-button>
+          </a>
         </template>
       </w-card>
       <w-divider color="white" class="my10" />
       <div>
         <span class="headline mb2">Lohnberechnungs-App (Schichtarbeit)</span>
-        <w-card :image="`${projectImg2}`" class="mt3" no-border>
+        <w-card class="mt3" no-border>
+          <img :src="projectImg3" class="mobile__gif" />
           <br />
           <span class="title2">Beschreibung:</span><br /><br />
           Diese Web-App dient dazu den Monatslohn anhand des Stundenlohns, unter
@@ -111,14 +136,23 @@
           </ul>
           <template #actions>
             <div class="spacer"></div>
-            <w-button class="mx5 mt5" xl bg-color="orange-light2" color="black"
-              ><w-icon class="mr2" xl color="black">mdi mdi-desktop-mac</w-icon>
-              Demo</w-button
+            <a
+              href="https://github.com/Mirza-bot/Lohn-Rechner"
+              rel="noopener"
+              target="_blank"
             >
-            <w-button class="mx8 mt5" xl bg-color="orange-light2" color="black"
-              ><w-icon class="mr2" xl color="black">mdi mdi-code-braces</w-icon
-              >Code</w-button
-            >
+              <w-button
+                class="mx8 mt5"
+                xl
+                bg-color="orange-light2"
+                color="black"
+              >
+                <w-icon class="mr2" xl color="black"
+                  >mdi mdi-code-braces</w-icon
+                >
+                Code</w-button
+              >
+            </a>
           </template>
         </w-card>
       </div>
@@ -129,8 +163,9 @@
           <br />
           <span class="title2">Beschreibung:</span><br /><br />
           Die Portfolio-Webseite auf der Sie sich befinden wurde mit JavaScript,
-          Vue.js, Vue-Router, Sass und Wave-UI erstellt. Dank des Vue-UI-Frameworks "Wave-UI"
-          ist die Umsetzung einer Webseite mit Responsive-Design keine Herausforderung. 
+          Vue.js, Vue-Router, Sass und Wave-UI erstellt. Dank des
+          Vue-UI-Frameworks "Wave-UI" ist die Umsetzung einer Webseite mit
+          Responsive-Design keine Herausforderung.
           <ul class="my5 description">
             <li>Responsive: Ja</li>
             <li>Art: Webseite</li>
@@ -139,18 +174,30 @@
           <span class="title2">Tools:</span>
           <ul class="mx5">
             <li><w-icon l>mdi mdi-language-javascript</w-icon>JavaScript</li>
-          <li><w-icon>mdi mdi-language-html5</w-icon>HTML5</li>
-          <li><w-icon l>mdi mdi-sass</w-icon>Sass</li>
-          <li><w-icon l>mdi mdi-vuejs</w-icon>Vue.js</li>
-          <li>Vue-Router</li>
-          <li>Wave-UI</li>
+            <li><w-icon>mdi mdi-language-html5</w-icon>HTML5</li>
+            <li><w-icon l>mdi mdi-sass</w-icon>Sass</li>
+            <li><w-icon l>mdi mdi-vuejs</w-icon>Vue.js</li>
+            <li>Vue-Router</li>
+            <li>Wave-UI</li>
           </ul>
           <template #actions>
             <div class="spacer"></div>
-            <w-button class="mx8 mt5" xl bg-color="orange-light2" color="black"
-              ><w-icon class="mr2" xl color="black">mdi mdi-code-braces</w-icon
-              >Code</w-button
+            <a
+              href="https://github.com/Mirza-bot/mein_portfolio"
+              rel="noopener"
+              target="_blank"
             >
+              <w-button
+                class="mx8 mt5"
+                xl
+                bg-color="orange-light2"
+                color="black"
+                ><w-icon class="mr2" xl color="black"
+                  >mdi mdi-code-braces</w-icon
+                >
+                Code</w-button
+              >
+            </a>
           </template>
         </w-card>
       </div>
@@ -163,7 +210,8 @@ export default {
   data: () => ({
     projectImg1: `./src/images/kanban.webp`,
     projectImg2: `./src/images/pietros pizzeria.webp`,
-    projectImg4: `./src/images/portfolio.webp`
+    projectImg3: `./src/images/lohnrechner.webp`,
+    projectImg4: `./src/images/portfolio.webp`,
   }),
 };
 </script>
@@ -187,6 +235,17 @@ div.page {
   }
   ul.description {
     list-style-type: none;
+  }
+}
+img.mobile__gif {
+  margin: auto auto 5vh 35vw;
+  max-width: 20vw;
+  border: 2px solid black;
+  border-radius: 60px;
+  @media only screen and (max-width: 1100px) {
+    margin: auto auto 5vh 12.5vw;
+    max-width: 60vw;
+    border-radius: 45px;
   }
 }
 </style>
